bb574e0ae31f1885accc8d0f5595a459cfce0c0a
ruslan-concero <ruslan@concero.io>
2024-06-25 00:38:03 +0500

    feat: sign up page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { useEffect } from "react"
+import { Navigator } from './Navigator'
 
 function App() {
 
 	useEffect(() => {
 		const urlParams = new URLSearchParams(window.location.search);
-		const ref = urlParams.get('ref');
-		const newRefParam = ref ? `?ref=${ref}` : "" 
+		const ref = urlParams.get('referral');
+		const newRefParam = ref ? `?referral=${ref}` : "" 
 
 		window.location.replace(`https://send.lanca.io/s/clxq1tsbr0000clnxl4mjkxzm${newRefParam}`)
 	}, [])
 
-	return null
+	return <Navigator />
 }
 
 export default App
--- src/Navigator.tsx
+++ src/Navigator.tsx
@@ -1,72 +1,36 @@
-import { lazy, Suspense, useEffect, useState } from 'react'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { AppScreen } from './components/screens/AppScreen/AppScreen'
-import { Header } from './components/layout/Header/Header/Header'
-import { routes } from './constants/routes'
-import { FullScreenLoader } from './components/layout/FullScreenLoader/FullScreenLoader'
-import { useAccount } from 'wagmi'
-import posthog from 'posthog-js'
 
-const SwapScreen = lazy(
-	async () =>
-		await import('./components/screens/SwapScreen/SwapScreen').then(module => ({ default: module.SwapScreen })),
-)
-const PortfolioScreen = lazy(
-	async () =>
-		await import('./components/screens/PortfolioScreen/PortfolioScreen').then(module => ({
-			default: module.PortfolioScreen,
-		})),
-)
-const EarnScreen = lazy(
-	async () =>
-		await import('./components/screens/EarnScreen/EarnScreen').then(module => ({ default: module.EarnScreen })),
-)
+
+// const SwapScreen = lazy(
+// 	async () =>
+// 		await import('./components/screens/SwapScreen/SwapScreen').then(module => ({ default: module.SwapScreen })),
+// )
+// const PortfolioScreen = lazy(
+// 	async () =>
+// 		await import('./components/screens/PortfolioScreen/PortfolioScreen').then(module => ({
+// 			default: module.PortfolioScreen,
+// 		})),
+// )
+// const EarnScreen = lazy(
+// 	async () =>
+// 		await import('./components/screens/EarnScreen/EarnScreen').then(module => ({ default: module.EarnScreen })),
+// )
 
 export const Navigator = () => {
-	const { address } = useAccount()
-	const [isNewSwapCardMode, setIsNewSwapCardMode] = useState(true)
-
-	useEffect(() => {
-		if (!address) return
-		posthog.identify(address)
-	}, [address])
 
 	return (
 		<BrowserRouter>
-			<AppScreen>
-				<Header isNewSwapCardMode={isNewSwapCardMode} setIsNewSwapCardMode={setIsNewSwapCardMode} />
 				<Routes>
 					<Route
-						path={routes.swap}
-						element={
-							<Suspense fallback={<FullScreenLoader />}>
-								<SwapScreen
-									isNewSwapCardMode={isNewSwapCardMode}
-									setIsNewSwapCardMode={setIsNewSwapCardMode}
-								/>
-							</Suspense>
-						}
+						path={'/signup'}
+						element={null}
 					/>
 					<Route
-						path={routes.portfolio}
-						element={
-							<Suspense fallback={<FullScreenLoader />}>
-								<PortfolioScreen />
-							</Suspense>
-						}
+						path={'/'}
+						element={null}
 					/>
-					<Route
-						path={routes.earn}
-						element={
-							<Suspense fallback={<FullScreenLoader />}>
-								<EarnScreen />
-							</Suspense>
-						}
-					/>
-					<Route path={routes.root} element={<Navigate to={routes.swap} />} />
-					<Route path={'/*'} element={<Navigate to={routes.swap} />} />
 				</Routes>
-			</AppScreen>
 		</BrowserRouter>
 	)
 }
