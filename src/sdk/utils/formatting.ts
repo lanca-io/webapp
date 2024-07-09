b3e76c6db03ebae1af9b839b18276551d61eaec9
lufaque <95128636+lufaque@users.noreply.github.com>
2024-07-09 12:49:58 +0300

    fix: execute route

--- src/sdk/utils/formatting.ts
+++ src/sdk/utils/formatting.ts
@@ -1,25 +1,4 @@
-import BigNumber from 'bignumber.js'
 import { parseUnits } from 'viem'
-
-export function isValidNumber(number: string | number) {
-	if (typeof number === 'string') {
-		return number !== ''
-	} else {
-		return !(number === undefined || number === null || isNaN(number))
-	}
-}
-
-export function addingAmountDecimals(number: number | string, decimals: number): string | null {
-	if (!isValidNumber(number) || !isValidNumber(decimals)) return null
-	let bNumber = new BigNumber(number)
-
-	while (!bNumber.isInteger()) {
-		bNumber = bNumber.times(10)
-		decimals--
-	}
-
-	return bNumber.toString() + '0'.repeat(decimals)
-}
 
 export const createBigIntAmount = (amount: string, decimals: number) => {
 	return parseUnits(amount, decimals)
