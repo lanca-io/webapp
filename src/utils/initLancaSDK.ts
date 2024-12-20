4a3de65d4891adab7fa170ad48c48ab2eb5a7f46
babamovandrej <andrejbabamov@gmail.com>
2024-12-20 13:44:11 +0100

    Update the lanca sdk pkg and use default config in initialization

--- src/utils/initLancaSDK.ts
+++ src/utils/initLancaSDK.ts
@@ -1,12 +1,3 @@
-import { LancaClient, type LancaClientConfig } from 'lanca-sdk-demo'
+import { LancaClient } from 'lanca-sdk-demo'
 
-const config: LancaClientConfig = {
-	chains: {
-		8453: ['https://base-rpc.publicnode.com', 'https://rpc.ankr.com/base'],
-		42161: ['https://arbitrum-one-rpc.publicnode.com', 'https://rpc.ankr.com/arbitrum'],
-		137: ['https://rpc.ankr.com/polygon', 'https://polygon-bor-rpc.publicnode.com'],
-		43114: ['https://avalanche-c-chain-rpc.publicnode.com', 'https://rpc.ankr.com/avalanche'],
-	},
-}
-
-export const lanca = new LancaClient(config)
+export const lanca = new LancaClient()
