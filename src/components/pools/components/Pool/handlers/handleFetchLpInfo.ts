1f2f6d418fbe1e16f363d411a3a4d2c92b237a05
babamovandrej <andrejbabamov@gmail.com>
2025-01-20 11:19:23 +0100

    Fix LP balance fetching

--- src/components/pools/components/Pool/handlers/handleFetchLpInfo.ts
+++ src/components/pools/components/Pool/handlers/handleFetchLpInfo.ts
@@ -4,45 +4,54 @@ import { config } from '../../../../../constants/config'
 import { getPoolLiquidity } from './handleFetchPoolLiquidity'
 import { abi as ParentPool } from '../../../config/abi/ParentPool.json'
 import { getPublicClient } from '../../../../../web3/wagmi'
-import { lpTokenBase } from '../../../config/poolMainnetAddresses'
-import { lpTokenBaseSepolia } from '../../../config/poolTestnetAddresses'
+import { lpTokenBase, parentPoolBase } from '../../../config/poolMainnetAddresses'
+import { lpTokenBaseSepolia, parentPoolBaseSepolia } from '../../../config/poolTestnetAddresses'
 
 const chainId = config.IS_TESTNET ? baseSepolia.id : base.id
 const lpAddress = config.IS_TESTNET ? lpTokenBaseSepolia : lpTokenBase
+const parentPool = config.IS_TESTNET ? parentPoolBaseSepolia : parentPoolBase
 const client = getPublicClient(chainId)
 const lpTokenDecimals = 18
 
-const readContract = async (address: Address, abi: any, functionName: string, args: any[] = []) => {
-	try {
-		return await client.readContract({ address, abi, functionName, args })
-	} catch (error) {
-		console.error(`Error reading contract function ${functionName} at address ${address}:`, error)
-		throw error
-	}
-}
-
 export const getUserLpTokens = async (userAddress: Address): Promise<number> => {
-	const lpTokens = await readContract(lpAddress, erc20Abi, 'balanceOf', [userAddress])
-	return Number(formatUnits(lpTokens as bigint, lpTokenDecimals))
+	const lpTokens = await client.readContract({
+		address: lpAddress,
+		abi: erc20Abi,
+		functionName: 'balanceOf',
+		args: [userAddress],
+	})
+
+	return Number(formatUnits(lpTokens, lpTokenDecimals))
 }
 
-export const getLpTotalSupply = async (): Promise<number> => {
-	const totalSupply = await readContract(lpAddress, erc20Abi, 'totalSupply')
-	return Number(formatUnits(totalSupply as bigint, lpTokenDecimals))
+export const getLpTotalSupply = async () => {
+	const totalSupply = await client.readContract({
+		address: lpAddress,
+		abi: erc20Abi,
+		functionName: 'totalSupply',
+	})
+
+	return Number(formatUnits(totalSupply, lpTokenDecimals))
 }
 
 export const calculateLpAmount = async (amountToDeposit: bigint): Promise<bigint> => {
 	const childPoolsBalance = await getPoolLiquidity(true)
-	return (await readContract(lpAddress, ParentPool, 'calculateLPTokensToMint', [
-		childPoolsBalance,
-		amountToDeposit,
-	])) as bigint
+
+	return (await client.readContract({
+		address: parentPool,
+		abi: ParentPool,
+		functionName: 'calculateLPTokensToMint',
+		args: [childPoolsBalance, amountToDeposit],
+	})) as bigint
 }
 
 export const calculateWithdrawableAmount = async (clpAmount: bigint): Promise<bigint> => {
 	const childPoolsBalance = await getPoolLiquidity(true)
-	return (await readContract(lpAddress, ParentPool, 'calculateWithdrawableAmount', [
-		childPoolsBalance,
-		clpAmount,
-	])) as bigint
+
+	return (await client.readContract({
+		address: parentPool,
+		abi: ParentPool,
+		functionName: 'calculateWithdrawableAmount',
+		args: [childPoolsBalance, clpAmount],
+	})) as bigint
 }
