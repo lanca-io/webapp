e154094f538217ed57bb69204c6c088a2155bf63
babamovandrej <andrejbabamov@gmail.com>
2025-02-11 20:07:15 +0100

    Change footer

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,13 +1,9 @@
-import { Concero } from '../../../assets/icons/Concero'
 import classNames from './Footer.module.pcss'
 
 export const Footer = () => {
 	return (
 		<footer className={classNames.footer}>
 			<a className={classNames.link} href="https://concero.io/" target="_blank" rel="noreferrer">
-				<div className={classNames.iconWrapper}>
-					<Concero />
-				</div>
 				<div className={classNames.textContent}>
 					<p>Powered by Concero</p>
 				</div>
