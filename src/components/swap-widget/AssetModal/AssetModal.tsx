e06be1607ba418849c140c692b56bf8e9d008052
babamovandrej <andrejbabamov@gmail.com>
2025-03-12 16:05:35 +0100

    Add initial token fetching

--- src/components/swap-widget/AssetModal/AssetModal.tsx
+++ src/components/swap-widget/AssetModal/AssetModal.tsx
@@ -2,11 +2,15 @@ import type { FC } from 'react'
 import type { AssetModalProps } from './types'
 import { Modal } from './Modal/Modal'
 import { ChainMenu } from './ChainMenu/ChainMenu'
+import { SearchBar } from './SearchBar/SearchBar'
+import { TokenMenu } from './TokenMenu/TokenMenu'
 
 export const AssetsModal: FC<AssetModalProps> = ({ isOpen, onClose }) => {
 	return (
 		<Modal title="Select a token and chain" isOpen={isOpen} onClose={onClose}>
+			<SearchBar />
 			<ChainMenu />
+			<TokenMenu />
 		</Modal>
 	)
 }
---
+++ src/components/swap-widget/AssetModal/SearchBar/SearchBar.tsx
@@ -0,0 +1,29 @@
+import type { FC } from 'react'
+import { useState, useEffect, useCallback } from 'react'
+import { TextInput } from '../../../layout/TextInput/TextInput'
+import { SearchIcon } from '../../../../assets/icons/SearchIcon'
+import { useTokensStore } from '../../../../store/tokens/useTokensStore'
+import { useDebounce } from '../../../../hooks/useDebounce'
+
+export const SearchBar: FC = () => {
+	const [input, setInput] = useState<string>('')
+	const debouncedInputValue = useDebounce(input, 800)
+	const { setSearchValue } = useTokensStore()
+
+	useEffect(() => {
+		setSearchValue(debouncedInputValue)
+	}, [debouncedInputValue, setSearchValue])
+
+	const handleChange = useCallback((value: string) => {
+		setInput(value)
+	}, [])
+
+	return (
+		<TextInput
+			placeholder="Search by name or paste address"
+			value={input}
+			onChangeText={handleChange}
+			icon={<SearchIcon />}
+		/>
+	)
+}
