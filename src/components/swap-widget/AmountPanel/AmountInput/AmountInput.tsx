7a5277678229044dc8b8c9f6c408082cdd159d89
babamovandrej <andrejbabamov@gmail.com>
2025-03-18 16:55:29 +0100

    Add preliminary amount inputs

--- src/components/swap-widget/AmountPanel/AmountInput/AmountInput.tsx
+++ src/components/swap-widget/AmountPanel/AmountInput/AmountInput.tsx
@@ -1,10 +1,55 @@
-import type { FC } from 'react'
+import type { FC, ChangeEvent } from 'react'
+import { useState, useRef } from 'react'
 import classNames from './AmountInput.module.pcss'
 
-export const AmountInput: FC = () => {
+interface AmountInputProps {
+	disabled?: boolean
+}
+
+export const AmountInput: FC<AmountInputProps> = ({ disabled }) => {
+	const [value, setValue] = useState<string>('')
+	const [placeholder, setPlaceholder] = useState<string>('0')
+	const ref = useRef<HTMLInputElement>(null)
+
+	const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+		const { value } = event.target
+		const formattedAmount = formatInputAmount(value)
+		setValue(formattedAmount)
+	}
+
+	const handleBlur = (event: ChangeEvent<HTMLInputElement>) => {
+		const { value } = event.target
+		const formattedAmount = formatInputAmount(value)
+		setValue(formattedAmount)
+		if (!formattedAmount) {
+			setPlaceholder('0')
+		}
+	}
+
+	const handleFocus = () => {
+		setPlaceholder('')
+	}
+
 	return (
-		<div className={classNames['amount-input']}>
-			<input type="text" className={classNames['input']} placeholder="0" />
+		<div className={classNames['amount-input-container']}>
+			<div className={classNames['amount-input']}>
+				<input
+					ref={ref}
+					type="text"
+					className={classNames['input']}
+					placeholder={placeholder}
+					value={value}
+					onChange={handleChange}
+					onBlur={handleBlur}
+					onFocus={handleFocus}
+					disabled={disabled}
+				/>
+			</div>
+			{!value && <div className={classNames['enter-amount']}>Enter amount</div>}
 		</div>
 	)
+}
+
+const formatInputAmount = (value: string): string => {
+	return value.replace(/[^0-9.]/g, '')
 }
