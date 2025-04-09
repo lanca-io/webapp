8da398d42253551dc205691f23f07c5af251af3b
babamovandrej <andrejbabamov@gmail.com>
2025-04-09 11:53:21 +0200

    Close pools and swap for infra update

--- src/api/lifi/executeLifiRoute.ts
+++ src/api/lifi/executeLifiRoute.ts
@@ -3,5 +3,6 @@ import { lifi } from './lifi'
 import { type Route } from '@lifi/types'
 import { type Signer } from 'ethers'
 
-export const executeLifiRoute = async (signer: Signer, route: Route, settings?: ExecutionSettings): Promise<Route> =>
-	await lifi.executeRoute(signer, route, settings)
+export const executeLifiRoute = async (signer: Signer, route: Route, settings?: ExecutionSettings): Promise<Route> => {
+	return lifi.executeRoute(signer, route, settings)
+}
