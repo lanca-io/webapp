13e300c4e5bb63dedd4d48676eef090b1dda12a6
babamovandrej <andrejbabamov@gmail.com>
2025-05-30 15:38:37 +0200

    Lanca V1

--- src/utils/retryRequest.ts
+++ src/utils/retryRequest.ts
@@ -2,7 +2,6 @@ interface Options {
 	retryCount?: number
 	throwCondition?: (e: any) => boolean
 }
-
 export async function retryRequest(request: (i: number) => Promise<any>, options: Options): Promise<any> {
 	const { retryCount = 3, throwCondition = () => false } = options
 	let error = 'Unknown error'
