dc8098980386fcc8baffcd98f767b7c9449a2a3c
babamovandrej <andrejbabamov@gmail.com>
2025-07-14 17:07:22 +0200

    Add back begin swap event, use FE RPC instead of testnet

--- src/providers/SDKProvider/SDKProvider.tsx
+++ src/providers/SDKProvider/SDKProvider.tsx
@@ -1,9 +1,10 @@
 import type { FC, PropsWithChildren } from 'react'
 import { LancaClient } from '@lanca/sdk'
 import { SDKContext } from './SDKContext'
+import { sdkConfiguration } from './ClientConfig'
 
 export const SDKProvider: FC<PropsWithChildren<{}>> = ({ children }) => {
-	const client = new LancaClient()
+	const client = new LancaClient(sdkConfiguration)
 
 	return <SDKContext.Provider value={{ client }}>{children}</SDKContext.Provider>
 }
