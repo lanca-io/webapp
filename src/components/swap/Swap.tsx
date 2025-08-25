4dc4c4d2db95539dc8210d382c241bd3b3771b2e
babamovandrej <andrejbabamov@gmail.com>
2025-08-25 09:49:41 +0200

    Close swap and pools

--- src/components/swap/Swap.tsx
+++ src/components/swap/Swap.tsx
@@ -1,9 +1,14 @@
 import type { FC } from 'react'
 import { SwapWidget } from '../common/SwapWidget/SwapWidget'
 import { ModalManager } from '../common/ModalManager/ModalManager'
+import { config } from '../../constants/config'
+import { TechWorksScreen } from '../screens/TechWorksScreen/TechWorksScreen'
 import './Swap.pcss'
 
 export const Swap: FC = () => {
+	if (config.APP_IS_NOT_AVAILABLE) {
+		return <TechWorksScreen />
+	}
 	return (
 		<div className="swap">
 			<SwapWidget />
