bc35f0aad55a6a3384f25b013bfffc0198636841
babamovandrej <andrejbabamov@gmail.com>
2025-09-05 13:22:31 +0200

    Remove console.logs

--- src/components/common/SourcePanel/SourcePanel.tsx
+++ src/components/common/SourcePanel/SourcePanel.tsx
@@ -10,16 +10,11 @@ import './SourcePanel.pcss'
 export const SourcePanel = memo((): JSX.Element => {
 	const { isConnected } = useAccount()
 	const { onChange, onFocus, onBlur } = useInputHandlers()
-	const { fromToken, amountInput, amountInputFocused, addressInputFocused, fromAmount } = useFormStore()
+	const { fromToken, amountInput, amountInputFocused, addressInputFocused } = useFormStore()
 
 	const hasBalance = Boolean(fromToken?.balance && fromToken.balance !== '0')
 	const isCompact = addressInputFocused && !amountInputFocused
 	const inputClassName = isCompact ? 'input-compact' : ''
-
-	console.log('From token passed to the BalanceInfo', fromToken)
-	console.log('Formatted token', Number(fromToken?.balance) / 10 ** (fromToken?.decimals ?? 18))
-	console.log('Amount input', amountInput)
-	console.log('From amount', fromAmount)
 
 	return (
 		<div className={`source_panel`}>
