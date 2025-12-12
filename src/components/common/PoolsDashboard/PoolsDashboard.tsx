9acc8d8f9370db965c14dd45657e624a1dfdd8dd
babamovandrej <andrejbabamov@gmail.com>
2025-12-12 10:36:45 +0100

    Finish statistics cards

--- src/components/common/PoolsDashboard/PoolsDashboard.tsx
+++ src/components/common/PoolsDashboard/PoolsDashboard.tsx
@@ -1,4 +1,5 @@
 import { VolumeChart } from '../VolumeChart/VolumeChart'
+import { StatisticsCard } from '../StatisticsCard/StatisticsCard'
 import './PoolsDashboard.pcss'
 
 export const PoolsDashboard = (): JSX.Element => {
@@ -6,6 +7,26 @@ export const PoolsDashboard = (): JSX.Element => {
 		<div className="pools_dashboard">
 			<div className="pools_analytics">
 				<VolumeChart />
+				<div className="pools_statistics">
+					<StatisticsCard
+						title="TXs"
+						value={128}
+						tooltip={{
+							title: 'Total Transactions',
+							description: 'Total number of transactions executed through Concero.',
+						}}
+						units="TX"
+					/>
+					<StatisticsCard
+						title="Providers"
+						value={128}
+						tooltip={{
+							title: 'Liquidity Providers',
+							description: 'Number of users currently providing liquidity to the pools.',
+						}}
+						units="Users"
+					/>
+				</div>
 			</div>
 		</div>
 	)
---
+++ src/components/common/StatisticsCard/StatisticsCard.tsx
@@ -0,0 +1,43 @@
+import type { FC } from 'react'
+import { Tooltip } from '../Tooltip/Tooltip'
+import { InfoIcon } from '@/assets/icons/InfoIcon'
+import './StatisticsCard.pcss'
+
+type StatisticsCardProps = {
+	title: string
+	value: number
+	units: string
+	tooltip: {
+		title: string
+		description: string
+	}
+}
+
+export const StatisticsCard: FC<StatisticsCardProps> = ({ title, value, units, tooltip }): JSX.Element => {
+	return (
+		<div className="statistics_card">
+			<div className="statistics_card_heading">
+				<span className="statistics_card_title">{title}</span>
+				<Tooltip
+					tooltipId={`statistics-card-tooltip-${title}`}
+					tooltipContent={
+						<div className="statistics_card_tooltip_content">
+							<span className="statistics_card_tooltip_title">{tooltip.title}</span>
+							<span className="statistics_card_tooltip_description">{tooltip.description}</span>
+						</div>
+					}
+					place="bottom"
+					className="statistics_card_tooltip"
+				>
+					<div className="statistics_card_tooltip_icon">
+						<InfoIcon color="var(--color-gray-600)" />
+					</div>
+				</Tooltip>
+			</div>
+			<div className="statistics_card_value_content">
+				<span className="statistics_card_value">{value}</span>
+				<span className="statistics_card_value_unit">{units}</span>
+			</div>
+		</div>
+	)
+}
