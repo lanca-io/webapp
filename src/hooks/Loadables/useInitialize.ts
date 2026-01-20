d3cf6035a10352d1e043999abb2e3b42e5c7a28f
babamovandrej <andrejbabamov@gmail.com>
2026-01-20 13:59:42 +0100

    Setup data fetching information

--- src/hooks/Loadables/useInitialize.ts
+++ src/hooks/Loadables/useInitialize.ts
@@ -3,6 +3,7 @@ import { useLoadAllTokens } from './useLoadAllTokens'
 import { useLoadTokens } from './useLoadTokens'
 import { useLoadTxExecutionTime } from './useLoadTxExecutionTime'
 import { useLoadRoute } from './useLoadRoute'
+import { useLoadPoolsData } from './useLoadPoolsData'
 
 export const InitializeLoadables = (): null => {
 	useLoadAllTokens()
@@ -10,5 +11,6 @@ export const InitializeLoadables = (): null => {
 	useLoadBalances()
 	useLoadTxExecutionTime()
 	useLoadRoute()
+	useLoadPoolsData()
 	return null
 }
---
+++ src/hooks/Loadables/useLoadPoolsData.ts
@@ -0,0 +1,36 @@
+import { useEffect } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import { usePoolsStore } from '@/store/pools/usePoolsStore'
+
+export const useLoadPoolsData = () => {
+	const { setMetrics, setIsLoading } = usePoolsStore()
+
+	const {
+		data,
+		isLoading: queryLoading,
+		refetch,
+	} = useQuery({
+		queryKey: ['poolsData'],
+		queryFn: async () => {
+			const response = await fetch(
+				'https://dev.concero.io/api/v1/pools?is_testnet=true',
+			)
+			if (!response.ok) throw new Error('Pools fetch failed')
+			const json = await response.json()
+			return json.payload
+		},
+		staleTime: 5 * 60_000,
+		retry: 2,
+		refetchOnWindowFocus: false,
+	})
+
+	useEffect(() => {
+		setIsLoading(queryLoading)
+		if (data) {
+			console.log('Pools payload:', data)
+			setMetrics(data.supply, data.cap, data.tvl)
+		}
+	}, [data, queryLoading, setMetrics, setIsLoading])
+
+	return { loading: queryLoading, refetch, data }
+}
