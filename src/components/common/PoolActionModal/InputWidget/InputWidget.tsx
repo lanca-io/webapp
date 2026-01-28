987cccb09d739104398a6ba30284e2abd946e33a
babamovandrej <andrejbabamov@gmail.com>
2026-01-28 09:39:25 +0100

    Add animation for modal

--- src/components/common/PoolActionModal/InputWidget/InputWidget.tsx
+++ src/components/common/PoolActionModal/InputWidget/InputWidget.tsx
@@ -2,21 +2,21 @@ import type { FC } from 'react'
 import { useMemo } from 'react'
 import { SourceCard } from './SourceCard/SourceCard'
 import { DestinationCard } from './DestinationCard/DestinationCard'
-import { PoolsActionType } from '../Reducer/types'
 import { ActionCard } from './ActionCard/ActionCard'
 import { Pointer } from './Pointer/Pointer'
+import { usePoolsActionContext } from '../Reducer/Provider'
 import './InputWidget.pcss'
 
 type InputFormProps = {
-	type: PoolsActionType
 	onClose: () => void
 }
 
-export const InputWidget: FC<InputFormProps> = ({ type, onClose }) => {
-	const source = useMemo(() => <SourceCard onClose={onClose} />, [type])
-	const destination = useMemo(() => <DestinationCard />, [type])
-	const action = useMemo(() => <ActionCard type={type} />, [type])
-	const pointer = useMemo(() => <Pointer />, [type])
+export const InputWidget: FC<InputFormProps> = ({ onClose }) => {
+	const { state } = usePoolsActionContext()
+	const source = useMemo(() => <SourceCard onClose={onClose} />, [state.type])
+	const destination = useMemo(() => <DestinationCard />, [state.type])
+	const action = useMemo(() => <ActionCard type={state.type} />, [state.type])
+	const pointer = useMemo(() => <Pointer />, [state.type])
 
 	return (
 		<div className="pool_action_input_widget">
